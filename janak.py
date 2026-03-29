fe223174c1b89116e794638efd5c400aacff26a6
Janak Shrestha <janakshresthabcs@gmail.com>
2026-03-29 17:16:27 +0200

    :

--- janak.py
+++ janak.py
@@ -1,3 +1,3 @@
-name = input("Enter your name")
-age = input("Enter your age")
+name = input("Enter your name:")
+age = input("Enter your age:")
 print("Your name is", name, "You are", age, "years old.")
